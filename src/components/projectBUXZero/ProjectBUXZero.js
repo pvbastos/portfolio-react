3dec31f10745fa37b75c9094329d7d23262ec0d1
Paulo Bastos <45849499+pvbastos@users.noreply.github.com>
2020-06-21 11:58:01 +0200

    Startet project page

--- src/components/projectBUXZero/ProjectBUXZero.js
+++ src/components/projectBUXZero/ProjectBUXZero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ProjectBUXZero.css';
+import BUXZeroVideo from '../../images/bux-zero-logo-animation.mp4'
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,6 +13,8 @@ const about = props => (
 
   <div className="wrapper-project" id="bux-zero">
     <div className="full-image">
+      <video src={BUXZeroVideo} width="100%" autoPlay loop mute></video>
+      
     </div>
     <div className="text-container">
     <p>BUX Zero is the app that allows people to buy and sell shares with zero commission. The purpose of the app is to make investing accessible to all audiences and this has to be reflected throughout the app's UI, visual identity and advertisements.</p>
